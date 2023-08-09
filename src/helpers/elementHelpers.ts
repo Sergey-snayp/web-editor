19426abf333be9b8d806d4ddf04d77195f06a5df
Sergey-snayp <59452160+Sergey-snayp@users.noreply.github.com>
2023-08-09 19:52:12 +0300

    updated findElementByCoordinates function

--- src/helpers/elementHelpers.ts
+++ src/helpers/elementHelpers.ts
@@ -1,21 +1,23 @@
 import { TElements } from '../interfaces/Elements';
 
-export function findElementByCoordinates(config: TElements, coordinates: number[], findLastInList?: string) {
+export function findElementByCoordinates(config: TElements, coordinates: number[], findLastElementInChain?: string) {
   let result: TElements = config;
-
-  if(findLastInList){
-    coordinates = coordinates.reverse();
-  }
+  let elementsHistory: TElements[]  = [];
 
   for (const coordinate of coordinates) {
     if (result && Array.isArray(result.children) && result.children[coordinate]) {
       result = result.children[coordinate];
-      if(findLastInList){
-        coordinates.shift();
-        if(findLastInList === result.elementType){
-          break;
+      elementsHistory.push(result);
+    }
+  }
+
+  if(findLastElementInChain){
+    elementsHistory = elementsHistory.reverse();
+
+    for (const element of elementsHistory) {
+        if(element.elementType === findLastElementInChain){
+          return element;
         }
-      }
     }
   }
 
